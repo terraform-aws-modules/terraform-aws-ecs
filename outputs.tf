a42662520e9d23873d272b12d907ab9ec52e4db8
Anton Babenko <anton@antonbabenko.com>
2019-03-05 21:39:08 +0100

    Added cluster name to outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "this_ecs_cluster_id" {
 output "this_ecs_cluster_arn" {
   value = "${element(concat(aws_ecs_cluster.this.*.arn, list("")), 0)}"
 }
+
+output "this_ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = "${var.name}"
+}
