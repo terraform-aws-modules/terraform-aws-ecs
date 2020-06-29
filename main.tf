81a0701fbd32644b3e5d0d0d4d9a14113a055c39
Anton Babenko <anton@antonbabenko.com>
2020-06-29 13:23:53 +0200

    Fixed code for 0.12

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_ecs_cluster" "this" {
   count = var.create_ecs ? 1 : 0
 
   name = var.name
-  setting = {
+
+  setting {
     name  = "containerInsights"
     value = var.container_insights ? "enabled" : "disabled"
   }
