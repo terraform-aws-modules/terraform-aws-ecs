c7614aa9d3153588ecc51b2835a9052b4394115a
Anton Babenko <anton@antonbabenko.com>
2021-04-26 22:22:51 +0200

    feat: Shorten outputs (removing this_) (#39)

--- modules/ecs-instance-profile/outputs.tf
+++ modules/ecs-instance-profile/outputs.tf
@@ -1,14 +1,14 @@
-output "this_iam_instance_profile_id" {
+output "iam_instance_profile_id" {
   description = "ID of the IAM instance profile"
   value       = aws_iam_instance_profile.this.id
 }
 
-output "this_iam_instance_profile_arn" {
+output "iam_instance_profile_arn" {
   description = "ARN of the IAM instance profile"
   value       = aws_iam_instance_profile.this.arn
 }
 
-output "this_iam_role_id" {
+output "iam_role_id" {
   description = "ID of the IAM role"
   value       = aws_iam_role.this.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "this_ecs_cluster_id" {
+output "ecs_cluster_id" {
   description = "ID of the ECS Cluster"
   value       = concat(aws_ecs_cluster.this.*.id, [""])[0]
 }
 
-output "this_ecs_cluster_arn" {
+output "ecs_cluster_arn" {
   description = "ARN of the ECS Cluster"
   value       = concat(aws_ecs_cluster.this.*.arn, [""])[0]
 }
 
-output "this_ecs_cluster_name" {
+output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
   value       = var.name
 }
