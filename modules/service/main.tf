f708c7cd6be5b321ec65bccc935b5baf99336340
Lays Rodrigues <laysrodriguessilva@gmail.com>
2024-03-04 14:16:07 -0300

    fix: Add missing Name tag in service sg
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -1320,7 +1320,7 @@ resource "aws_security_group" "this" {
   description = var.security_group_description
   vpc_id      = data.aws_subnet.this[0].vpc_id
 
-  tags = merge(var.tags, var.security_group_tags)
+  tags = merge(var.tags, var.security_group_tags, { Name : local.security_group_name })
 
   lifecycle {
     create_before_destroy = true
