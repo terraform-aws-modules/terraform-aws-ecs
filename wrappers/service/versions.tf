c83dc9e810cdd1df65b861572f2170e505380510
matt <97546619+mattaltberg@users.noreply.github.com>
2024-05-31 13:28:06 -0400

    feat: Add support for custom metric queries in customized_metric_spec (#196)
    * feat: add support for custom metric queries in customized metric spec
    
    * fix: Update and run pre-commit checks to format
    
    ---------
    
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- wrappers/service/versions.tf
+++ wrappers/service/versions.tf
@@ -1,10 +1,18 @@
 terraform {
+<<<<<<< HEAD
   required_version = ">= 1.0"
+=======
+  required_version = ">= 1.3"
+>>>>>>> edf0446 (feat: Add support for custom metric queries in customized_metric_spec (#196))
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+<<<<<<< HEAD
       version = ">= 4.66.1"
+=======
+      version = ">= 5.34"
+>>>>>>> edf0446 (feat: Add support for custom metric queries in customized_metric_spec (#196))
     }
   }
 }
