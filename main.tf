01fdd62c834d2ed6dd3bcab65d8611f20ea89414
Bryant Biggs <bryantbiggs@gmail.com>
2024-07-05 13:51:06 -0400

    fix: Change placement to be alphabetical

--- main.tf
+++ main.tf
@@ -108,9 +108,9 @@ module "service" {
   family                        = try(each.value.family, null)
   inference_accelerator         = try(each.value.inference_accelerator, {})
   ipc_mode                      = try(each.value.ipc_mode, null)
-  pid_mode                      = try(each.value.pid_mode, null)
   memory                        = try(each.value.memory, 2048)
   network_mode                  = try(each.value.network_mode, "awsvpc")
+  pid_mode                      = try(each.value.pid_mode, null)
   proxy_configuration           = try(each.value.proxy_configuration, {})
   requires_compatibilities      = try(each.value.requires_compatibilities, ["FARGATE"])
   runtime_platform = try(each.value.runtime_platform, {
