f7cd82e1ed43d9dceceb76263490b9d230b50470
Rizal Diantoro <m.diantoro@gooddoctor.co.id>
2024-09-12 17:19:56 +0700

    fix

--- modules/service/outputs.tf
+++ modules/service/outputs.tf
@@ -4,12 +4,12 @@
 
 output "id" {
   description = "ARN that identifies the service"
-  value       = try(aws_ecs_service.this[0].id, aws_ecs_service.ignore_task_definition[0].id, null)
+  value       = try(aws_ecs_service.this[0].id, aws_ecs_service.ignore_task_definition[0].id, aws_ecs_service.codedeploy[0].id, null)
 }
 
 output "name" {
   description = "Name of the service"
-  value       = try(aws_ecs_service.this[0].name, aws_ecs_service.ignore_task_definition[0].name, null)
+  value       = try(aws_ecs_service.this[0].name, aws_ecs_service.ignore_task_definition[0].name, aws_ecs_service.codedeploy[0].name, null)
 }
 
 ################################################################################
