b1e5be8ecd61e3bd808e1b2cfa5207acd7fac298
Richard Gavel <richard@ordinaryexperts.com>
2024-11-13 14:43:55 -0600

    Add cloudwatch log class functionality

--- modules/container-definition/main.tf
+++ modules/container-definition/main.tf
@@ -76,6 +76,7 @@ resource "aws_cloudwatch_log_group" "this" {
 
   name              = var.cloudwatch_log_group_use_name_prefix ? null : local.log_group_name
   name_prefix       = var.cloudwatch_log_group_use_name_prefix ? "${local.log_group_name}-" : null
+  log_group_class   = var.cloudwatch_log_group_log_group_class
   retention_in_days = var.cloudwatch_log_group_retention_in_days
   kms_key_id        = var.cloudwatch_log_group_kms_key_id
 
