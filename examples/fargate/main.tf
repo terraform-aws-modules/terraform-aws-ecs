terraform-aws-modules/terraform-aws-ecs

commit 78c170242dbafd468874dd5c7325c907e56b6b06

docs: Add examples for open issue questions (#188)

examples/fargate/main.tf
@@ -121,6 +121,12 @@ module "ecs_service" {
         }
       }
 
+      # Not required for fluent-bit, just an example
+      volumes_from = [{
+        sourceContainer = "fluent-bit"
+        readOnly        = false
+      }]
+
       memory_reservation = 100
     }
   }
@@ -179,13 +185,17 @@ module "ecs_task_definition" {
   source = "../../modules/service"
 
   # Service
-  name           = "${local.name}-standalone"
-  cluster_arn    = module.ecs_cluster.arn
-  create_service = false
+  name        = "${local.name}-standalone"
+  cluster_arn = module.ecs_cluster.arn
 
   # Task Definition
   volume = {
     ex-vol = {}
+  }
+
+  runtime_platform = {
+    cpu_architecture        = "ARM64"
+    operating_system_family = "LINUX"
   }
 
   # Container definition(s)
@@ -200,7 +210,8 @@ module "ecs_task_definition" {
         }
       ]
 
-      command = ["/usr/bin/cat", "/etc/os-release"]
+      command    = ["echo hello world"]
+      entrypoint = ["/usr/bin/sh", "-c"]
     }
   }
 

.pre-commit-config.yaml
@@ -1,6 +1,6 @@
 repos:
   - repo: https://github.com/antonbabenko/pre-commit-terraform
-    rev: v1.88.1
+    rev: v1.88.4
     hooks:
       - id: terraform_fmt
       - id: terraform_wrapper_module_for_each
@@ -25,7 +25,7 @@ repos:
           - '--args=--only=terraform_unused_required_providers'
       - id: terraform_validate
   - repo: https://github.com/pre-commit/pre-commit-hooks
-    rev: v4.5.0
+    rev: v4.6.0
     hooks:
       - id: check-merge-conflict
       - id: end-of-file-fixer

commit d10c206c1e9273475e889b80b884d6d3368ec311

create_service false for stand alone task
Regression bug after #188

examples/fargate/main.tf
@@ -185,8 +185,9 @@ module "ecs_task_definition" {
   source = "../../modules/service"
 
   # Service
-  name        = "${local.name}-standalone"
-  cluster_arn = module.ecs_cluster.arn
+  name           = "${local.name}-standalone"
+  cluster_arn    = module.ecs_cluster.arn
+  create_service = false
 
   # Task Definition
   volume = {