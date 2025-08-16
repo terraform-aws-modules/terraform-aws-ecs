f1fdccb3618563caacef20efb10d248e1cd2f7ce
Melissa Greenbaum <69476188+magreenbaum@users.noreply.github.com>
2025-08-16 16:06:50 -0400

    fix: Update `tls.issuer_cert_authority` dynamic block for `aws_ecs_service.ignore_task_definition` (#341)
    fix service connect tls dynamic block

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -571,7 +571,7 @@ resource "aws_ecs_service" "ignore_task_definition" {
 
             content {
               dynamic "issuer_cert_authority" {
-                for_each = tls.value.issuer_cert_authority
+                for_each = [tls.value.issuer_cert_authority]
 
                 content {
                   aws_pca_authority_arn = issuer_cert_authority.value.aws_pca_authority_arn
