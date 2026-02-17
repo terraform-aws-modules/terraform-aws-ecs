97cf8ab0dd1026498bf01abef0d28cdd858d3641
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2026-02-17 09:12:41 +0100

    feat: Add option to ignore `container_definitions` changes to ECS task definition

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -920,7 +920,7 @@ locals {
 }
 
 resource "aws_ecs_task_definition" "this" {
-  count = local.create_task_definition ? 1 : 0
+  count = local.create_task_definition && !var.ignore_container_definition_changes ? 1 : 0
 
   region = var.region
 
@@ -1050,6 +1050,145 @@ resource "aws_ecs_task_definition" "this" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+################################################################################
+# Ignore Task Definition
+################################################################################
+
+resource "aws_ecs_task_definition" "ignore_container_definition" {
+  count = local.create_task_definition && var.ignore_container_definition_changes ? 1 : 0
+
+  region = var.region
+
+  # Convert map of maps to array of maps before JSON encoding
+  container_definitions  = jsonencode([for k, v in module.container_definition : v.container_definition])
+  cpu                    = var.cpu
+  enable_fault_injection = var.enable_fault_injection
+
+  dynamic "ephemeral_storage" {
+    for_each = var.ephemeral_storage != null ? [var.ephemeral_storage] : []
+
+    content {
+      size_in_gib = ephemeral_storage.value.size_in_gib
+    }
+  }
+
+  execution_role_arn = try(aws_iam_role.task_exec[0].arn, var.task_exec_iam_role_arn)
+  family             = coalesce(var.family, var.name)
+
+  ipc_mode     = var.ipc_mode
+  memory       = var.memory
+  network_mode = var.network_mode
+  pid_mode     = var.pid_mode
+
+  dynamic "placement_constraints" {
+    for_each = var.task_definition_placement_constraints != null ? var.task_definition_placement_constraints : {}
+
+    content {
+      expression = placement_constraints.value.expression
+      type       = placement_constraints.value.type
+    }
+  }
+
+  dynamic "proxy_configuration" {
+    for_each = var.proxy_configuration != null ? [var.proxy_configuration] : []
+
+    content {
+      container_name = proxy_configuration.value.container_name
+      properties     = proxy_configuration.value.properties
+      type           = proxy_configuration.value.type
+    }
+  }
+
+  requires_compatibilities = var.requires_compatibilities
+
+  dynamic "runtime_platform" {
+    for_each = var.runtime_platform != null ? [var.runtime_platform] : []
+
+    content {
+      cpu_architecture        = runtime_platform.value.cpu_architecture
+      operating_system_family = runtime_platform.value.operating_system_family
+    }
+  }
+
+  skip_destroy  = var.skip_destroy
+  task_role_arn = try(aws_iam_role.tasks[0].arn, var.tasks_iam_role_arn)
+  track_latest  = var.track_latest
+
+  dynamic "volume" {
+    for_each = var.volume != null ? var.volume : {}
+
+    content {
+      configure_at_launch = volume.value.configure_at_launch
+
+      dynamic "docker_volume_configuration" {
+        for_each = volume.value.docker_volume_configuration != null ? [volume.value.docker_volume_configuration] : []
+
+        content {
+          autoprovision = docker_volume_configuration.value.autoprovision
+          driver        = docker_volume_configuration.value.driver
+          driver_opts   = docker_volume_configuration.value.driver_opts
+          labels        = docker_volume_configuration.value.labels
+          scope         = docker_volume_configuration.value.scope
+        }
+      }
+
+      dynamic "efs_volume_configuration" {
+        for_each = volume.value.efs_volume_configuration != null ? [volume.value.efs_volume_configuration] : []
+
+        content {
+          dynamic "authorization_config" {
+            for_each = efs_volume_configuration.value.authorization_config != null ? [efs_volume_configuration.value.authorization_config] : []
+
+            content {
+              access_point_id = authorization_config.value.access_point_id
+              iam             = authorization_config.value.iam
+            }
+          }
+
+          file_system_id          = efs_volume_configuration.value.file_system_id
+          root_directory          = efs_volume_configuration.value.root_directory
+          transit_encryption      = efs_volume_configuration.value.transit_encryption
+          transit_encryption_port = efs_volume_configuration.value.transit_encryption_port
+        }
+      }
+
+      dynamic "fsx_windows_file_server_volume_configuration" {
+        for_each = volume.value.fsx_windows_file_server_volume_configuration != null ? [volume.value.fsx_windows_file_server_volume_configuration] : []
+
+        content {
+          dynamic "authorization_config" {
+            for_each = fsx_windows_file_server_volume_configuration.value.authorization_config != null ? [fsx_windows_file_server_volume_configuration.value.authorization_config] : []
+
+            content {
+              credentials_parameter = authorization_config.value.credentials_parameter
+              domain                = authorization_config.value.domain
+            }
+          }
+
+          file_system_id = fsx_windows_file_server_volume_configuration.value.file_system_id
+          root_directory = fsx_windows_file_server_volume_configuration.value.root_directory
+        }
+      }
+
+      host_path = volume.value.host_path
+      name      = coalesce(volume.value.name, volume.key)
+    }
+  }
+
+  tags = merge(var.tags, var.task_tags)
+
+  depends_on = [
+    aws_iam_role_policy_attachment.tasks,
+    aws_iam_role_policy_attachment.task_exec,
+    aws_iam_role_policy_attachment.task_exec_additional,
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [container_definitions]
   }
 }
 
