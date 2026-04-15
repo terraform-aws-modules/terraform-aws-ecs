13b9f1f396234aac0309f66fde16ad4c16308473
Stas Davydov <davidovsv@yandex.ru>
2026-04-16 01:35:55 +0400

    feat: Add support for s3files volume (#1)
    * Added support for s3files volume.
    * Updated aws provider version requirement.

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -1056,6 +1056,16 @@ resource "aws_ecs_task_definition" "this" {
         }
       }
 
+      dynamic "s3files_volume_configuration" {
+        for_each = volume.value.s3files_volume_configuration != null ? [volume.value.s3files_volume_configuration] : []
+
+        content {
+          file_system_arn  = s3files_volume_configuration.value.file_system_arn
+          root_directory   = s3files_volume_configuration.value.root_directory
+          access_point_arn = s3files_volume_configuration.value.access_point_arn
+        }
+      }
+
       host_path = volume.value.host_path
       name      = coalesce(volume.value.name, volume.key)
     }
